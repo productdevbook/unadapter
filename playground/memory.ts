ac0a0f000bc7b1004e55d9ae2f0b050115730aea
productdevbook <hi@productdevbook.com>
2025-04-18 19:01:09 +0300

    refactor: restructure getTables function to be part of createAdapter options

--- playground/memory.ts
+++ playground/memory.ts
@@ -5,62 +5,78 @@ const db = {
   user: [],
   session: [],
 }
+// Initialize the adapter
 
-export function getTables(options?: AdapterOptions) {
-  return {
-    user: {
-      modelName: 'user',
-      fields: {
-        name: {
-          type: 'string',
-          required: true,
-          fieldName: options?.user?.fields?.name || 'name',
-          sortable: true,
-        },
-        email: {
-          type: 'string',
-          unique: true,
-          required: true,
-          fieldName: options?.user?.fields?.email || 'email',
-          sortable: true,
-        },
-        emailVerified: {
-          type: 'boolean',
-          defaultValue: () => false,
-          required: true,
-          fieldName: options?.user?.fields?.emailVerified || 'emailVerified',
-        },
-        image: {
-          type: 'string',
-          required: false,
-          fieldName: options?.user?.fields?.image || 'image',
-        },
-        createdAt: {
-          type: 'date',
-          defaultValue: () => new Date(),
-          required: true,
-          fieldName: options?.user?.fields?.createdAt || 'createdAt',
-        },
-        updatedAt: {
-          type: 'date',
-          defaultValue: () => new Date(),
-          required: true,
-          fieldName: options?.user?.fields?.updatedAt || 'updatedAt',
-        },
-      },
-    },
-  } satisfies UnDbSchema
+interface CreateAdapter<
+  T extends Record<string, any>,
+  Schema extends UnDbSchema = UnDbSchema,
+  Models extends Record<string, any> = Record<string, any>,
+> {
+  tables: (options: AdapterOptions) => Schema
 }
 
-// Initialize the adapter
-const createAdapter = memoryAdapter(
-  db,
-  getTables,
-  {},
-)
+function createAdapter<
+  T extends Record<string, any>,
+  Schema extends UnDbSchema = UnDbSchema,
+  Models extends Record<string, any> = Record<string, any>,
+>(options: CreateAdapter<T, Schema, Models>) {
+  const createAdapter = memoryAdapter(
+    db,
+    options.tables,
+    {},
+  )
+  // Create an adapter instance
+  const adapter = createAdapter({})
+  return adapter
+}
 
-// Create an adapter instance
-const adapter = createAdapter({})
+const adapter = createAdapter({
+  tables: (options?: AdapterOptions) => {
+    return {
+      user: {
+        modelName: 'user',
+        fields: {
+          name: {
+            type: 'string',
+            required: true,
+            fieldName: options?.user?.fields?.name || 'name',
+            sortable: true,
+          },
+          email: {
+            type: 'string',
+            unique: true,
+            required: true,
+            fieldName: options?.user?.fields?.email || 'email',
+            sortable: true,
+          },
+          emailVerified: {
+            type: 'boolean',
+            defaultValue: () => false,
+            required: true,
+            fieldName: options?.user?.fields?.emailVerified || 'emailVerified',
+          },
+          image: {
+            type: 'string',
+            required: false,
+            fieldName: options?.user?.fields?.image || 'image',
+          },
+          createdAt: {
+            type: 'date',
+            defaultValue: () => new Date(),
+            required: true,
+            fieldName: options?.user?.fields?.createdAt || 'createdAt',
+          },
+          updatedAt: {
+            type: 'date',
+            defaultValue: () => new Date(),
+            required: true,
+            fieldName: options?.user?.fields?.updatedAt || 'updatedAt',
+          },
+        },
+      },
+    } satisfies UnDbSchema
+  },
+})
 
 // eslint-disable-next-line antfu/no-top-level-await
 const user = await adapter.create({
